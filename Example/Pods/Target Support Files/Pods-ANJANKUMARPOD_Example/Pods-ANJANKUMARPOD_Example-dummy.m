7c4513c7a9cf5c4c9880fa385c9254304db3e37a
PotturiAnjanaKumar <anjanpotturi@gmail.com>
2022-07-29 00:29:58 +0530

    Update1
    Update1

--- Example/Pods/Target Support Files/Pods-ANJANKUMARPOD_Example/Pods-ANJANKUMARPOD_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ANJANKUMARPOD_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ANJANKUMARPOD_Example
-@end
---
+++ KUMARPOD/KUMARPOD.h
@@ -0,0 +1,18 @@
+//
+//  KUMARPOD.h
+//  KUMARPOD
+//
+//  Created by ANJANA KUMAR on 29/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KUMARPOD.
+FOUNDATION_EXPORT double KUMARPODVersionNumber;
+
+//! Project version string for KUMARPOD.
+FOUNDATION_EXPORT const unsigned char KUMARPODVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KUMARPOD/PublicHeader.h>
+
+
